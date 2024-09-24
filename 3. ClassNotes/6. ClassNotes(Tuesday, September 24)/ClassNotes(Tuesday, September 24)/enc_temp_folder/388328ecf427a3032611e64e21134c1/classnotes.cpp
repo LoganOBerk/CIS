26cac7dff18de5e157d0d3e2330f9d28a28e21ad
LoganOBerk <146039828+LoganOBerk@users.noreply.github.com>
2024-09-24 12:18:08 -0400

    classnotes september 24

---
+++ 3. ClassNotes/6. ClassNotes(Tuesday, September 24)/ClassNotes(Tuesday, September 24)/enc_temp_folder/388328ecf427a3032611e64e21134c1/classnotes.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+
+
+//char** argv can be written as char *argv[]
+int main(int argc, const char **argv) { //used to pass arguments into main argc = arg count, argv = arg value
+	int args = 1;
+
+	cout << "The program is called " << *argv << endl; //gives address from windows
+	//argc counts the arguments inside the debugger
+	cout << "It has " << argc - 1 << " arguments:\n";
+
+	while (args < argc) {
+		cout << *(argv + args) << endl;
+		++args;
+	}
+
+	//pointers arrays
+	const int SIZE = 5;
+	int number[SIZE];
+	//all the same starts at index 0
+	cout << "number :" << number;              
+	cout << "&number :" << &number;
+	cout << "&number[0]" << &number[0];
+
+	//incrementing addresses
+	int* P, x[10];
+	P = &x[3];
+	P++; //moves to next address
+
+	
+	for (int x = 0; x < SIZE; ++x) {
+		//array notation
+		cout << number[x];
+	}
+
+	
+	for (int x = 0; x < SIZE; ++x) {
+		//array offset notation uses arrayname
+		cout << *(number + x);
+	}
+
+	
+	for (int x = 0; x < SIZE; ++x, P++) {
+		//pointer notation 
+		cout << *P;
+		cout << *P++;
+	}
+
+	
+	for (int x = 0; x < SIZE; x++) {
+		//pointer offset notation uses specific pointer
+		cout << *(P + x);
+	}
+
+	return 0;
+};
