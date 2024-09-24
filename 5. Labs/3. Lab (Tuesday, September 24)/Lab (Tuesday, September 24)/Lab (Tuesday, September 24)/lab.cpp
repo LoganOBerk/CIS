2860ee43c84f848d86e6cf14d38aaae54c061b35
LoganOBerk <146039828+LoganOBerk@users.noreply.github.com>
2024-09-24 19:48:12 -0400

    finished lab3

--- 5. Labs/3. Lab (Tuesday, September 24)/Lab (Tuesday, September 24)/Lab (Tuesday, September 24)/lab.cpp
+++ 5. Labs/3. Lab (Tuesday, September 24)/Lab (Tuesday, September 24)/Lab (Tuesday, September 24)/lab.cpp
@@ -1,102 +1,192 @@
+/************************
+* Programer Name: Logan Berk
+*
+* Purpose: Show how pointers can be used, args can be used, and how arrays can be accessed differently with pointers
+*
+* Create Date: 9/24/2024
+*
+* Modified Date: 9/24/2024
+*************************/
+
+
 #include <iostream>
 #include <random>
 
 using namespace std;
 
+void question1();
+void question2();
+
 int main(int argc, const char *argv[]) {
 
+	question1();
+	question2();
+
+	cout << "------------------------------------------------------------------------------" << endl;
+	cout << "Printing Arguments/File path to .exe file" << endl;
+	cout << "------------------------------------------------------------------------------" << endl;
+	cout << "Executible : " << *argv << endl;
+	if (argc == 1) {
+		cout << "There are 0 arguments at the moment." << endl;
+	}
+	for (int i = 1; i < argc; i++) {
+		cout << "Argument " << i << ": " << argv[i] << endl;
+	}
+
+
+	return 0;
+};
+
+void question1() {
 	int X[] = { 13, 17, 21, 27, 33 };
 	int* xPtr = &X[2];
-	//xPtr address location is moved back one and then derefrenced
+
 	cout << "------------------------------------------------------------------------------" << endl;
 	cout << "Array content prior to pointer manipulation\n";
 	cout << X[1] << ' ' << X[2] << ' ' << X[3] << ' ' << xPtr << " : " << *xPtr << endl;
-	cout << *--xPtr;
+	cout << "\tExpected : " << 17 << endl;
+	cout << "\tActual   : " << *--xPtr;
 	cout << endl << "Array content after ptr manipulation" << endl;
 	cout << X[1] << ' ' << X[2] << ' ' << X[3] << ' ' << xPtr << " : " << *xPtr << endl;
-	cout << "------------------------------------------------------------------------------" << endl;
+	cout << "\tExpected : " << 17 << endl;
+	cout << "\tActual   : " << *xPtr << endl;
 
-	//Here we are derefrencing the -- operator which results in compiler error
+	X[0] = 13;
+	X[1] = 17;
+	X[2] = 21;
+	X[3] = 27;
+	X[4] = 33;
+	xPtr = &X[2];
+
+	cout << "------------------------------------------------------------------------------" << endl;
+	cout << "Array content prior to pointer manipulation\n";
+	cout << X[1] << ' ' << X[2] << ' ' << X[3] << ' ' << xPtr << " : " << *xPtr << endl;
+	cout << "\tExpected : " << 17 << endl;
+	cout << "\tActual   : " << *(--xPtr);
+	cout << endl << "Array content after ptr manipulation" << endl;
+	cout << X[1] << ' ' << X[2] << ' ' << X[3] << ' ' << xPtr << " : " << *xPtr << endl;
+	cout << "\tExpected : " << 17 << endl;
+	cout << "\tActual   : " << *xPtr << endl;
+
+	X[0] = 13;
+	X[1] = 17;
+	X[2] = 21;
+	X[3] = 27;
+	X[4] = 33;
+	xPtr = &X[2];
+
+	cout << "------------------------------------------------------------------------------" << endl;
+	cout << "Array content prior to pointer manipulation\n";
+	cout << X[1] << ' ' << X[2] << ' ' << X[3] << ' ' << xPtr << " : " << *xPtr << endl;
+	cout << "\tExpected : " << 20 << endl;
+	cout << "\tActual   : " << -- * xPtr;
+	cout << endl << "Array content after ptr manipulation" << endl;
+	cout << X[1] << ' ' << X[2] << ' ' << X[3] << ' ' << xPtr << " : " << *xPtr << endl;
+	cout << "\tExpected : " << 20 << endl;
+	cout << "\tActual   : " << *xPtr << endl;
+
+	X[0] = 13;
+	X[1] = 17;
+	X[2] = 21;
+	X[3] = 27;
+	X[4] = 33;
+	xPtr = &X[2];
+
+	cout << "------------------------------------------------------------------------------" << endl;
+	cout << "Array content prior to pointer manipulation\n";
+	cout << X[1] << ' ' << X[2] << ' ' << X[3] << ' ' << xPtr << " : " << *xPtr << endl;
+	cout << "\tExpected : " << 20 << endl;
+	cout << "\tActual   : " << --(*xPtr);
+	cout << endl << "Array content after ptr manipulation" << endl;
+	cout << X[1] << ' ' << X[2] << ' ' << X[3] << ' ' << xPtr << " : " << *xPtr << endl;
+	cout << "\tExpected : " << 20 << endl;
+	cout << "\tActual   : " << *xPtr << endl;
+
 	/*
 	cout << "Array content prior to pointer manipulation\n";
 	cout << X[1] << ' ' << X[2] << ' ' << X[3] << ' ' << xPtr << " : " << *xPtr << endl;
-	cout << (*--)xPtr;
+	cout << (*--)xPtr; //compiler error
+	cout << endl << "Array content after ptr manipulation" << endl;
+	cout << X[1] << ' ' << X[2] << ' ' << X[3] << ' ' << xPtr << " : " << *xPtr << endl;
+
+
+	cout << "Array content prior to pointer manipulation\n";
+	cout << X[1] << ' ' << X[2] << ' ' << X[3] << ' ' << xPtr << " : " << *xPtr << endl;
+	cout << (--*)xPtr; //compiler error
 	cout << endl << "Array content after ptr manipulation" << endl;
 	cout << X[1] << ' ' << X[2] << ' ' << X[3] << ' ' << xPtr << " : " << *xPtr << endl;
 	*/
 
-
-	//Here we are derefrencing xPtr and as a result we get our 3rd position 21 and then decrementing by 1 so we get 20
-	cout << "------------------------------------------------------------------------------" << endl;
-	cout << "Array content prior to pointer manipulation\n";
-	cout << X[1] << ' ' << X[2] << ' ' << X[3] << ' ' << xPtr << " : " << *xPtr << endl;
-	cout << -- * xPtr;
-	cout << endl << "Array content after ptr manipulation" << endl;
-	cout << X[1] << ' ' << X[2] << ' ' << X[3] << ' ' << xPtr << " : " << *xPtr << endl;
-	cout << "------------------------------------------------------------------------------" << endl;
-
-	//Here we are derefrencing xPtr and as a result we get our 3rd position 21 and then decrementing by 1 so we get 20
-	cout << "------------------------------------------------------------------------------" << endl;
-	cout << "Array content prior to pointer manipulation\n";
-	cout << X[1] << ' ' << X[2] << ' ' << X[3] << ' ' << xPtr << " : " << *xPtr << endl;
-	cout << --(*xPtr);
-	cout << endl << "Array content after ptr manipulation" << endl;
-	cout << X[1] << ' ' << X[2] << ' ' << X[3] << ' ' << xPtr << " : " << *xPtr << endl;
-	cout << "------------------------------------------------------------------------------" << endl;
-
-	//Here we are decrementing the star operator resulting in a compiler error
-	/*
-	cout << "Array content prior to pointer manipulation\n";
-	cout << X[1] << ' ' << X[2] << ' ' << X[3] << ' ' << xPtr << " : " << *xPtr << endl;
-	cout << (--*)xPtr;
-	cout << endl << "Array content after ptr manipulation" << endl;
-	cout << X[1] << ' ' << X[2] << ' ' << X[3] << ' ' << xPtr << " : " << *xPtr << endl;
-	*/
-
-	//Here we are derefrencing xPtr and then decrementing after the cout statement so our output is 21 and after its 20
-	cout << "------------------------------------------------------------------------------" << endl;
-	cout << "Array content prior to pointer manipulation\n";
-	cout << X[1] << ' ' << X[2] << ' ' << X[3] << ' ' << xPtr << " : " << *xPtr << endl;
-	cout << *xPtr--;
-	cout << endl << "Array content after ptr manipulation" << endl;
-	cout << X[1] << ' ' << X[2] << ' ' << X[3] << ' ' << xPtr << " : " << *xPtr << endl;
-	cout << "------------------------------------------------------------------------------" << endl;
-
-	//Here we are derefrencing xPtr and then decrementing after the cout statement so our output is 21 and after its 20
-	cout << "------------------------------------------------------------------------------" << endl;
-	cout << "Array content prior to pointer manipulation\n";
-	cout << X[1] << ' ' << X[2] << ' ' << X[3] << ' ' << xPtr << " : " << *xPtr << endl;
-	cout << (*xPtr)--;
-	cout << endl << "Array content after ptr manipulation" << endl;
-	cout << X[1] << ' ' << X[2] << ' ' << X[3] << ' ' << xPtr << " : " << *xPtr << endl;
-	cout << "------------------------------------------------------------------------------" << endl;
-
-	//Here we are derefrencing xPtr and then decrementing after the cout statement so our output is 21 and after its 20
-	cout << "------------------------------------------------------------------------------" << endl;
-	cout << "Array content prior to pointer manipulation\n";
-	cout << X[1] << ' ' << X[2] << ' ' << X[3] << ' ' << xPtr << " : " << *xPtr << endl;
-	cout << *(xPtr)--;
-	cout << endl << "Array content after ptr manipulation" << endl;
-	cout << X[1] << ' ' << X[2] << ' ' << X[3] << ' ' << xPtr << " : " << *xPtr << endl;
-	cout << "------------------------------------------------------------------------------" << endl;
-	
-	//Here we are derefrencing 
-	cout << "------------------------------------------------------------------------------" << endl;
-	cout << "Array content prior to pointer manipulation\n";
-	cout << X[1] << ' ' << X[2] << ' ' << X[3] << ' ' << xPtr << " : " << *xPtr << endl;
-	cout << *(xPtr--);
-	cout << endl << "Array content after ptr manipulation" << endl;
-	cout << X[1] << ' ' << X[2] << ' ' << X[3] << ' ' << xPtr << " : " << *xPtr << endl;
-	cout << "------------------------------------------------------------------------------" << endl;
+	X[0] = 13;
+	X[1] = 17;
+	X[2] = 21;
+	X[3] = 27;
+	X[4] = 33;
+	xPtr = &X[2];
 
 	cout << "------------------------------------------------------------------------------" << endl;
 	cout << "Array content prior to pointer manipulation\n";
 	cout << X[1] << ' ' << X[2] << ' ' << X[3] << ' ' << xPtr << " : " << *xPtr << endl;
-	cout << *(--xPtr);
+	cout << "\tExpected : " << 21 << endl;
+	cout << "\tActual   : " << *xPtr--;
 	cout << endl << "Array content after ptr manipulation" << endl;
 	cout << X[1] << ' ' << X[2] << ' ' << X[3] << ' ' << xPtr << " : " << *xPtr << endl;
-	cout << "------------------------------------------------------------------------------" << endl;
+	cout << "\tExpected : " << 17 << endl;
+	cout << "\tActual   : " << *xPtr << endl;
 
+	X[0] = 13;
+	X[1] = 17;
+	X[2] = 21;
+	X[3] = 27;
+	X[4] = 33;
+	xPtr = &X[2];
+
+	cout << "------------------------------------------------------------------------------" << endl;
+	cout << "Array content prior to pointer manipulation\n";
+	cout << X[1] << ' ' << X[2] << ' ' << X[3] << ' ' << xPtr << " : " << *xPtr << endl;
+	cout << "\tExpected : " << 21 << endl;
+	cout << "\tActual   : " << (*xPtr)--;
+	cout << endl << "Array content after ptr manipulation" << endl;
+	cout << X[1] << ' ' << X[2] << ' ' << X[3] << ' ' << xPtr << " : " << *xPtr << endl;
+	cout << "\tExpected : " << 20 << endl;
+	cout << "\tActual   : " << *xPtr << endl;
+
+	X[0] = 13;
+	X[1] = 17;
+	X[2] = 21;
+	X[3] = 27;
+	X[4] = 33;
+	xPtr = &X[2];
+
+	cout << "------------------------------------------------------------------------------" << endl;
+	cout << "Array content prior to pointer manipulation\n";
+	cout << X[1] << ' ' << X[2] << ' ' << X[3] << ' ' << xPtr << " : " << *xPtr << endl;
+	cout << "\tExpected : " << 21 << endl;
+	cout << "\tActual   : " << *(xPtr)--;
+	cout << endl << "Array content after ptr manipulation" << endl;
+	cout << X[1] << ' ' << X[2] << ' ' << X[3] << ' ' << xPtr << " : " << *xPtr << endl;
+	cout << "\tExpected : " << 17 << endl;
+	cout << "\tActual   : " << *xPtr << endl;
+
+	X[0] = 13;
+	X[1] = 17;
+	X[2] = 21;
+	X[3] = 27;
+	X[4] = 33;
+	xPtr = &X[2];
+
+	cout << "------------------------------------------------------------------------------" << endl;
+	cout << "Array content prior to pointer manipulation\n";
+	cout << X[1] << ' ' << X[2] << ' ' << X[3] << ' ' << xPtr << " : " << *xPtr << endl;
+	cout << "\tExpected : " << 21 << endl;
+	cout << "\tActual   : " << *(xPtr--);
+	cout << endl << "Array content after ptr manipulation" << endl;
+	cout << X[1] << ' ' << X[2] << ' ' << X[3] << ' ' << xPtr << " : " << *xPtr << endl;
+	cout << "\tExpected : " << 17 << endl;
+	cout << "\tActual   : " << *xPtr << endl;
+	cout << "------------------------------------------------------------------------------" << endl;
+}
+void question2() {
 	int nums[100];
 	int* numsPtr = nums;
 	const int MIN = -39;
@@ -105,21 +195,32 @@ int main(int argc, const char *argv[]) {
 	random_device rd;
 	mt19937 gen(rd());
 	uniform_int_distribution<> distrib(MIN, MAX);
+
+	//Filling with random numbers
 	for (int i = 0; i < 100; i++) {
 		int rand = distrib(gen);
 		nums[i] = rand;
 	}
-
-	for (int i = 0; i < 100; i++) {
-		cout << *(nums + i) << " ";
-	}
-	cout << endl << endl;
+	cout << "------------------------------------------------------------------------------" << endl;
+	cout << "Array Notation print" << endl;
+	cout << "------------------------------------------------------------------------------" << endl;
 
 	for (int i = 0; i < 100; i++) {
 		cout << nums[i] << " ";
 	}
 	cout << endl << endl;
 
+	cout << "------------------------------------------------------------------------------" << endl;
+	cout << "Array Offset Notation print" << endl;
+	cout << "------------------------------------------------------------------------------" << endl;
+	for (int i = 0; i < 100; i++) {
+		cout << *(nums + i) << " ";
+	}
+	cout << endl << endl;
+
+	cout << "------------------------------------------------------------------------------" << endl;
+	cout << "Pointer Notation print negative numbers" << endl;
+	cout << "------------------------------------------------------------------------------" << endl;
 	for (int i = 0; i < 100; i++, numsPtr++) {
 		if (*numsPtr < 0) {
 			cout << *numsPtr << " ";
@@ -129,7 +230,10 @@ int main(int argc, const char *argv[]) {
 		}
 	}
 	cout << endl << endl;
-	
+
+	cout << "------------------------------------------------------------------------------" << endl;
+	cout << "Pointer Offset Notation print numbers greater than 100" << endl;
+	cout << "------------------------------------------------------------------------------" << endl;
 	numsPtr = nums;
 	for (int i = 0; i < 100; i++) {
 		if (*(numsPtr + i) >= 100) {
@@ -140,12 +244,4 @@ int main(int argc, const char *argv[]) {
 		}
 	}
 	cout << endl << endl;
-
-	cout << "Executible : " << *argv << endl;
-	for (int i = 1; i < argc; i++) {
-		cout << "Argument " << i << " " << argv[i] << endl;
-	}
-
-
-	return 0;
-};
+}
