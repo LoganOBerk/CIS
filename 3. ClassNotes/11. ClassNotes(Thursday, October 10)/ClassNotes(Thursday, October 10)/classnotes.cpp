e0f1fa46559d40749bc15401d6e1f1fb7d1b2ead
LoganOBerk <146039828+LoganOBerk@users.noreply.github.com>
2024-10-10 12:15:30 -0400

    classnotes 11

---
+++ 3. ClassNotes/11. ClassNotes(Thursday, October 10)/ClassNotes(Thursday, October 10)/classnotes.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+//Software development 
+//Cost of a change curve
+//The more time it takes to make a change the more it costs
+//make sure instructions are clear and complete
+// 
+//Data dictionary
+//	defines all names(variables) used and how they relate to each other
+//	Usually stored in spreadsheet or database
+// 
+//Input-Output Mapping Document
+//	How do the inputs and outputs relate to eachother?
+//	Usually stored in a spreadsheet or data base
+//
+//Test Strategy
+//	High level testing strategy or plan that will PROVE
+//	the program has been written correctly.  This is NOT
+//	every test case that will be executed
+//
+//Algorithm
+//	Logic to follow
+//
+//Test Plan
+//	Actual cases to test and record results
+//
+//example data dictionary
+//Type: Data
+//Data Type : Integer
+//Source : Calculated
+//Used in : Program_1
+//Calculation : positiveIntegerOne * positiveIntegerTwo
+//
+//Algorithm
+//	Very specific psuedo code
+//
+//Software Development Planning
+//	Three legged stool
+//		-Time
+//		-Cost
+//		-Quality
+//
+//		-Content
+
+	return 0;
+}
