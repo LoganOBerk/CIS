e8729333158814b718e4a1765ab52ae6d9aa6372
LoganOBerk <146039828+LoganOBerk@users.noreply.github.com>
2024-10-24 12:16:41 -0400

    class

---
+++ 3. ClassNotes/14. ClassNotes(Thursday, October 24)/ClassNotes(Thursday, October 24)/ClassNotes(Thursday, October 24)/classnotes.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+
+int main() {
+
+	//ABSTRACTION
+	//	the more generic it is the more abstract it is
+	//OOAD
+	//	Object oriented analysis and design
+	//		process for solving problems
+	//
+	//cohesion
+	//	Each module should perform one well defined task
+	//		well named and self documented
+	//
+	//coupling
+	//	dependance or intedependance between functions
+	//		ideally we want more cohesion and loose coupling
+	//		a strongly coupled function or class is one that is highly dependant on another
+	//		
+	//Operation Contracts
+	//	Documents
+	//		how method can be used
+	//		what limitations does it have
+	//	Specify
+	//		purpose
+	//		data flow
+	//		pre condition, post-condition, input
+	//
+	//Abstraction
+	//	Seperate purpose of something with its implimentation
+	//
+	//Specifications do not indicate how to implement
+	//Up to you
+	//Types
+	//Characteristics
+	//
+	//information hiding
+	//
+	//INTERFACES
+	//	complete interface for a class allows programmer to accomplish any reasonable task
+	//	minimal interface for a class contains method iff that method is essential to class's responsibilities
+	//
+	//an ADT is an abstract data type and its a collection of data and a set of operations on data
+	//a data structure is an implimentation of an ADT within a programming language
+	//
+	//ADT bag contains anything you place in it
+	//A CRC card is Class Responsibility Collaboration
+	//	responsibilities
+	//		what it does
+	//	collaborations
+	//		what it allows
+	// 
+	// UML (unified modeling language)
+	//	9 different diagrams for describing a program
+	//	software engineer
+	//	UML is the way to communicate software to be developed
+	// 
+	// 
+	// UML
+	// Generalization
+	// DOG -|> ANIMAL
+	// the arrow goes from one UML to another and represents the highest level is the parent
+	// Aggregation
+	// COMPUTER <>-> CPU
+	// The diamond with arrow shows that it contains this
+	// Assosiation
+	// WINDOW -> CURSOR
+	//
+	//Use Cases and Scenarios
+	//	Use case consists of multiple scenarios
+	return 0;
+}
