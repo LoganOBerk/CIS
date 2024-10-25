9c303321a176e970584985ff2ddf8ac005b403f8
LoganOBerk <146039828+LoganOBerk@users.noreply.github.com>
2024-10-25 11:51:04 -0400

    added utility files for userInput

--- 6. Practice/2. UsefulFuncs/UsefulFuncs/useful_funcs.h
+++ 6. Practice/2. UsefulFuncs/UsefulFuncs/useful_funcs.h
@@ -1,150 +1,19 @@
 #ifndef USEFUL_FUNCS_H
 #define USEFUL_FUNCS_H
-
-
-#include <algorithm>
-#include <cctype>
-#include <iomanip>
-#include <iostream>
-#include <map>
-#include <string>
-#include <type_traits>
-#include <regex>
-#include <sstream>
-#include <limits>
-#include <regex>
-
-using namespace std;
-
-//Regex namespace initialization
-namespace RegexPatterns{
-    extern const regex scientificNotation;
-    extern const regex floatingPoint;
-    extern const regex integral;
-
-}
-
-
-
-//String consts initialization
-extern const string IS_ALPHA;
-extern const string IS_NUMERIC;
-extern const string IS_FILE;
-extern const string IS_CHARACTER;
-extern const string IS_NULL;
-
-//Numeric consts initialization
-extern const int      MAX_INT;
-extern const int      MIN_INT;
-extern const long     MAX_LONG;
-extern const long     MIN_LONG;
-extern const unsigned MAX_UNSIGNED;
-extern const unsigned MIN_UNSIGNED;
-extern const float    MAX_FLOAT;
-extern const float    MIN_FLOAT;
-extern const double   MAX_DOUBLE;
-extern const double   MIN_DOUBLE;
-
-
-//Boolean consts initialization
-extern const bool   ONE_VALUE;
-extern const bool   MULTI_VALUE;
-extern const bool   CLEAR_BUFFER;
-extern const bool   DONT_CLEAR_BUFFER;
-extern const bool   CASE_SENSITIVE;
-extern const bool   NOT_CASE_SENSITIVE;
-
-
-//Helper function prototypes
-bool isFileChar(const char);
-bool isDigit(const char);
-bool isAlpha(const char);
-string formatAndTrim(string&);
-void initialInputHandling(string&, const bool&, const bool&, const bool&);
-bool validateSegments(const string&, const regex&);
-int findSigFigLength(const string&, const bool&, int&);
-//void checkForValidNumericalBools(const bool&, const bool&);
+#include "userInput_utils.h"
 
 //Usefull func prototypes
 string pullWord(string&, const int);
 
-
-template<typename T>
-string typeName() {
-    return typeid(T).name();
-}
-
-
-//Insure programmer does not input incorrect booleans with certain data
-template <typename T>
-void checkForValidBools(const bool& singleInput, const bool& caseSensitive) {
-    if (!singleInput) {
-        throw invalid_argument("ARGUMENT: singleInput MUST BE TRUE FOR: " + typeName<T>() + "!");
-    }
-    if (caseSensitive) {
-        throw invalid_argument("ARGUMENT: caseSensitive MUST BE FALSE FOR: " + typeName<T>() + "!");
-    }
-}
-
-
-
-//Validates floats
-template<typename T>
-typename enable_if<is_same<T, float>::value, T>::type
-validatedFloatingPoint(string& input, const T& param1, const T& param2, int& decimal, int& length) {
-    const int sigFig = numeric_limits<T>::digits10;
-    T numConvert = 0;
-    try {
-        numConvert = stof(input);
-    }
-    catch (const out_of_range&) {
-        throw out_of_range("You entered a float much too large or small!");
-    }
-
-    if (numConvert < param1 || numConvert > param2) {
-        throw out_of_range("You entered a number outside of the range ( "
-            + to_string(param1) + ", " + to_string(param2) + " )");
-    }
-    else if (length > sigFig) {
-        throw out_of_range("You entered a number with more than " + to_string(sigFig) + " significant figures!");
-    }
-    return numConvert;
-}
-
-//Validates doubles
-template<typename T>
-typename enable_if<is_same<T, double>::value, T>::type
-validatedFloatingPoint(string& input, const T& param1, const T& param2, int& decimal, int& length) {
-    const int sigFig = numeric_limits<T>::digits10;
-    T numConvert = 0;
-    try {
-        numConvert = stod(input);
-    }
-    catch (const out_of_range&) {
-        throw out_of_range("You entered a number much too large or small!");
-    }
-
-    if (numConvert < param1 || numConvert > param2) {
-        throw out_of_range("You entered a number outside of the range ( "
-            + to_string(param1) + ", " + to_string(param2) + " )");
-    }
-    else if (length > sigFig) {
-        throw out_of_range("You entered a number with more than " + to_string(sigFig) + " significant figures!");
-    }
-    return numConvert;
-}
-
-
-
-
+//USERINPUT
 //Default template if no type is matched
 template<typename T, typename Enable = void>
 inline typename enable_if<!is_integral<T>::value, T>::type
 userInput(string&, const T&, const T&, const bool&, const bool&, const bool&) {
-    static_assert(is_same<T, void>::value, "Invalid type for userInput");
+    static_assert(is_same<T, void>::value, "Invalid type for userInput!");
 }
 
-
+//USERINPUT
 //Specialization for integral types
 template<typename T>
 inline typename enable_if<is_integral<T>::value, T>::type
@@ -159,7 +28,7 @@ userInput(string& input, const T& param1, const T& param2, const bool& singleInp
    
     try {
         if (hasSpaces && allIntegers && stold(input) == static_cast<T>(stold(input))) {
-            throw invalid_argument("You entered more than one input.");
+            throw invalid_argument("You entered more than one input!");
         }
         else if (!isScientific && !allIntegers                //evaluates true if the data isnt scientific or isnt an int
             || stold(input) != static_cast<T>(stold(input))   //checks for scientific data thats non-int
@@ -185,6 +54,7 @@ userInput(string& input, const T& param1, const T& param2, const bool& singleInp
 }
 
 
+//USERINPUT
 //Specialization for float
 template<>
 inline float userInput<float>(string& input, const float& param1, const float& param2, const bool& singleInput, const bool& isClearBuffer, const bool& caseSensitive) {
@@ -200,7 +70,7 @@ inline float userInput<float>(string& input, const float& param1, const float& p
     for (const auto ch : input) {
         if (!isDigit(ch)) {
             if (hasSpaces && allFloatingPointNums) {
-                throw invalid_argument("You entered more than one input.");
+                throw invalid_argument("You entered more than one input!");
             }
             else if (ch == '.') {
                 decimal++;
@@ -221,6 +91,7 @@ inline float userInput<float>(string& input, const float& param1, const float& p
 }
 
 
+//USERINPUT
 //Specialization for double
 template<>
 inline double userInput<double>(string& input, const double& param1, const double& param2, const bool& singleInput, const bool& isClearBuffer, const bool& caseSensitive) {
@@ -236,7 +107,7 @@ inline double userInput<double>(string& input, const double& param1, const doubl
     for (const auto ch : input) {
         if (!isDigit(ch)) {
             if (hasSpaces && allFloatingPointNums) {
-                throw invalid_argument("You entered more than one input.");
+                throw invalid_argument("You entered more than one input!");
             }
             else if (ch == '.') {
                 decimal++;
@@ -255,6 +126,7 @@ inline double userInput<double>(string& input, const double& param1, const doubl
 }
 
 
+//USERINPUT
 //Specialization for string
 template<>
 inline string userInput<string>(string& input, const string& param1, const string& param2, const bool& singleInput, const bool& isClearBuffer, const bool& caseSensitive) {
@@ -300,6 +172,7 @@ inline string userInput<string>(string& input, const string& param1, const strin
 }
 
 
+//USERINPUT
 //Specialization for bool
 template<>
 inline bool userInput<bool>(string& input, const bool& param1, const bool& param2, const bool& singleInput, const bool& isClearBuffer, const bool& caseSensitive) {
--- 6. Practice/2. UsefulFuncs/UsefulFuncs/userInput_utils.h
+++ 6. Practice/2. UsefulFuncs/UsefulFuncs/userInput_utils.h
@@ -1,1 +1,136 @@
-#pragma once
+#ifndef USERINPUT_UTILS_H
+#define USERINPUT_UTILS_H
+
+
+#include <algorithm>
+#include <cctype>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <string>
+#include <type_traits>
+#include <regex>
+#include <sstream>
+#include <limits>
+#include <regex>
+
+using namespace std;
+
+//Regex namespace initialization
+namespace RegexPatterns {
+    extern const regex scientificNotation;
+    extern const regex floatingPoint;
+    extern const regex integral;
+
+}
+
+
+
+//String consts initialization
+extern const string IS_ALPHA;
+extern const string IS_NUMERIC;
+extern const string IS_FILE;
+extern const string IS_CHARACTER;
+extern const string IS_NULL;
+
+//Numeric consts initialization
+extern const int      MAX_INT;
+extern const int      MIN_INT;
+extern const long     MAX_LONG;
+extern const long     MIN_LONG;
+extern const unsigned MAX_UNSIGNED;
+extern const unsigned MIN_UNSIGNED;
+extern const float    MAX_FLOAT;
+extern const float    MIN_FLOAT;
+extern const double   MAX_DOUBLE;
+extern const double   MIN_DOUBLE;
+
+
+//Boolean consts initialization
+extern const bool   ONE_VALUE;
+extern const bool   MULTI_VALUE;
+extern const bool   CLEAR_BUFFER;
+extern const bool   DONT_CLEAR_BUFFER;
+extern const bool   CASE_SENSITIVE;
+extern const bool   NOT_CASE_SENSITIVE;
+
+
+//Helper function prototypes
+bool isFileChar(const char);
+bool isDigit(const char);
+bool isAlpha(const char);
+string formatAndTrim(string&);
+void initialInputHandling(string&, const bool&, const bool&, const bool&);
+bool validateSegments(const string&, const regex&);
+int findSigFigLength(const string&, const bool&, int&);
+
+
+
+
+template<typename T>
+string typeName() {
+    return typeid(T).name();
+}
+
+
+//Insure programmer does not input incorrect booleans with certain data
+template <typename T>
+void checkForValidBools(const bool& singleInput, const bool& caseSensitive) {
+    if (!singleInput) {
+        throw invalid_argument("ARGUMENT: singleInput MUST BE TRUE FOR: " + typeName<T>() + "!");
+    }
+    if (caseSensitive) {
+        throw invalid_argument("ARGUMENT: caseSensitive MUST BE FALSE FOR: " + typeName<T>() + "!");
+    }
+}
+
+
+
+//Validates floats
+template<typename T>
+typename enable_if<is_same<T, float>::value, T>::type
+validatedFloatingPoint(string& input, const T& param1, const T& param2, int& decimal, int& length) {
+    const int sigFig = numeric_limits<T>::digits10;
+    T numConvert = 0;
+    try {
+        numConvert = stof(input);
+    }
+    catch (const out_of_range&) {
+        throw out_of_range("You entered a number much too large or small!");
+    }
+
+    if (numConvert < param1 || numConvert > param2) {
+        throw out_of_range("You entered a number outside of the range ( "
+            + to_string(param1) + ", " + to_string(param2) + " )");
+    }
+    else if (length > sigFig) {
+        throw out_of_range("You entered a number with more than " + to_string(sigFig) + " significant figures!");
+    }
+    return numConvert;
+}
+
+
+//Validates doubles
+template<typename T>
+typename enable_if<is_same<T, double>::value, T>::type
+validatedFloatingPoint(string& input, const T& param1, const T& param2, int& decimal, int& length) {
+    const int sigFig = numeric_limits<T>::digits10;
+    T numConvert = 0;
+    try {
+        numConvert = stod(input);
+    }
+    catch (const out_of_range&) {
+        throw out_of_range("You entered a number much too large or small!");
+    }
+
+    if (numConvert < param1 || numConvert > param2) {
+        throw out_of_range("You entered a number outside of the range ( "
+            + to_string(param1) + ", " + to_string(param2) + " )");
+    }
+    else if (length > sigFig) {
+        throw out_of_range("You entered a number with more than " + to_string(sigFig) + " significant figures!");
+    }
+    return numConvert;
+}
+
+#endif
