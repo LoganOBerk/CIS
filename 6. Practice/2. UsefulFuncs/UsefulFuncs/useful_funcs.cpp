bad259e12202bfafa61971ae3a08f9c607f286b3
LoganOBerk <146039828+LoganOBerk@users.noreply.github.com>
2024-10-28 23:08:25 -0400

    added clarity to pull value function

--- 6. Practice/2. UsefulFuncs/UsefulFuncs/useful_funcs.cpp
+++ 6. Practice/2. UsefulFuncs/UsefulFuncs/useful_funcs.cpp
@@ -3,20 +3,25 @@
 //Takes an input identifies values and allows accessing values in sentances by word number
 string pullValue(string& input, int valNum) {
     int amountOfValues;
+    string initialValue;
+    string finalValue;
     if (--valNum < 0) {
         throw out_of_range("You cannot access less than 1 value!");
     }
     
     amountOfValues = numVals(input);
+    initialValue = input.substr(0, input.find(' '));
+    finalValue = input.substr(input.rfind(' ') + 1, input.size() - 1);
+
     vector<string> values;
-    values.push_back(input.substr(0, input.find(' ')));
-    for (int i = values[0].size(); i < input.size(); i++) {
+    values.push_back(initialValue);
+    for (int i = initialValue.size(); i < input.size(); i++) {
         if (input[i] == ' ') {
             values.push_back(input.substr(i + 1, input.substr(i + 1).find(' ')));
         }
     }
     if (amountOfValues > 1) {
-        values.push_back(input.substr(input.rfind(' ') + 1, input.size() - 1));
+        values.push_back(finalValue);
     }
     if (valNum >= amountOfValues) {
         throw out_of_range("Trying to access value number " + to_string(valNum + 1) + " when there are only " + to_string(amountOfValues) + " values!");
