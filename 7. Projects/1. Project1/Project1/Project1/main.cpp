650fb968e88d744b75298af5db1f640470cbd250
LoganOBerk <146039828+LoganOBerk@users.noreply.github.com>
2024-10-30 03:40:03 -0400

    finished project1

--- 7. Projects/1. Project1/Project1/Project1/main.cpp
+++ 7. Projects/1. Project1/Project1/Project1/main.cpp
@@ -1,48 +1,129 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
 #include "myString.h"
 
 using namespace std;
 
+void logAction(ofstream& logFile, const string& methodName, const string& originalValue,
+    const string& parameters, const string& modifiedValue,
+    const string& statusMessage) {
+    logFile << left << setw(25) << methodName
+        << setw(25) << originalValue
+        << setw(25) << parameters
+        << setw(25) << modifiedValue
+        << statusMessage << endl;
+}
+
+void writeResultsToFile(myString* str, const string& result, ofstream& outFile) {
+    outFile << result << endl;
+}
+
+void printFileContents(const string& fileName) {
+    ifstream file(fileName);
+    if (!file) {
+        cerr << "Unable to open file: " << fileName << endl;
+        return;
+    }
+    string line;
+    cout << "Contents of " << fileName << ":" << endl;
+    while (getline(file, line)) {
+        cout << line << endl;
+    }
+    file.close();
+}
+
 int main() {
-	Status myStatus;
-	myString* str1 = new myString(myStatus, "Hello");
-	myString* str2 = new myString(myStatus, " World ");
-	myString* str3 = new myString(myStatus, " Universe ");
-		str1 = str1->replWholeString(*str2);
-		str1->printStringScreen();
+    Status myStatus;
 
-		str1 = str1->setString("apple");
-		str2 = str2->setString("apricot");
-		cout << str1->compareString(*str2);
-		cout << endl;
+    ofstream outFile("output.txt");
+    ofstream logFile("log.txt");
 
-		str1 = str1->setString("Hello");
-		str2 = str2->setString("Hi");
-		str1 = str1->replPartString(*str2, 1, 4);
-		str1->printStringScreen();
+    logFile << left << setw(25) << "Method Called"
+        << setw(25) << "Original Value"
+        << setw(25) << "Parameters"
+        << setw(25) << "Modified Value"
+        << "Status" << endl;
 
-		str1 = str1->setString("Hello");
-		str1 = str1->partString(1, 5);
-		str1->printStringScreen();
+    myString* str1 = new myString(myStatus, "Hello");
+    myString* str2 = new myString(myStatus, " World ");
 
-		str1->initString();
-		str1->printStringScreen();
+    string originalValue = str1->getString();
+    str1 = str1->replWholeString(*str2);
+    logAction(logFile, "replWholeString", originalValue, " World ", str1->getString(), "Success");
+    writeResultsToFile(str1, str1->getString(), outFile);
+    str1->printStringScreen();
 
-		str1 = str1->setString("Hello");
-		str2 = str2->setString(" World ");
-		str1->addEnd(*str2);
-		str1->printStringScreen();
+    str1 = str1->setString("apple");
+    str2 = str2->setString("apricot");
+    originalValue = str1->getString();
+    string comparisonResult = to_string(str1->compareString(*str2));
+    logAction(logFile, "compareString", originalValue, "apricot", comparisonResult, "Success");
+    writeResultsToFile(str1, comparisonResult, outFile);
+    cout << comparisonResult << endl;
 
-		str1 = str1->setString("Hello");
-		str1->addStart(*str2);
-		str1->printStringScreen();
+    str1 = str1->setString("Hello");
+    str2 = str2->setString("Hi");
+    originalValue = str1->getString();
+    str1 = str1->replPartString(*str2, 1, 4);
+    logAction(logFile, "replPartString", originalValue, "Hi, 1, 4", str1->getString(), "Success");
+    writeResultsToFile(str1, str1->getString(), outFile);
+    str1->printStringScreen();
 
-		str1 = str1->setString("Hello");
-		cout << str1->getString() << endl;
+    str1 = str1->setString("Hello");
+    originalValue = str1->getString();
+    str1 = str1->partString(1, 3);
+    logAction(logFile, "partString", originalValue, "1, 3", str1->getString(), "Success");
+    writeResultsToFile(str1, str1->getString(), outFile);
+    str1->printStringScreen();
 
-		cout << str1->numericString() << endl;
-		cout << str1->alphabeticString() << endl;
+    str1->initString();
+    originalValue = str1->getString();
+    logAction(logFile, "initString", originalValue, "", str1->getString(), "Success");
+    writeResultsToFile(str1, str1->getString(), outFile);
+    str1->printStringScreen();
 
+    str1 = str1->setString("Hello");
+    str2 = str2->setString(" World ");
+    originalValue = str1->getString();
+    str1->addEnd(*str2);
+    logAction(logFile, "addEnd", originalValue, str2->getString(), str1->getString(), "Success");
+    writeResultsToFile(str1, str1->getString(), outFile);
+    str1->printStringScreen();
 
+    str1 = str1->setString("Hello");
+    originalValue = str1->getString();
+    str1->addStart(*str2);
+    logAction(logFile, "addStart", originalValue, str2->getString(), str1->getString(), "Success");
+    writeResultsToFile(str1, str1->getString(), outFile);
+    str1->printStringScreen();
 
-	return 0;
+    string strValue = str1->getString();
+    originalValue = str1->getString();
+    logAction(logFile, "getString", originalValue, "", strValue, "Success");
+    writeResultsToFile(str1, strValue, outFile);
+    cout << strValue << endl;
+
+    string numericResult = to_string(str1->numericString());
+    logAction(logFile, "numericString", originalValue, "", numericResult, "Success");
+    writeResultsToFile(str1, numericResult, outFile);
+    cout << numericResult << endl;
+
+    string alphabeticResult = to_string(str1->alphabeticString());
+    logAction(logFile, "alphabeticString", originalValue, "", alphabeticResult, "Success");
+    writeResultsToFile(str1, alphabeticResult, outFile);
+    cout << alphabeticResult << endl;
+
+    delete str1;
+    delete str2;
+
+    outFile.close();
+    logFile.close();
+    cout << endl;
+
+    printFileContents("output.txt");
+    cout << endl;
+    printFileContents("log.txt");
+
+    return 0;
 }
--- 7. Projects/1. Project1/Project1/Project1/myString.cpp
+++ 7. Projects/1. Project1/Project1/Project1/myString.cpp
@@ -1,210 +1,216 @@
 #include "myString.h"
 
-myString::myString(Status& extStatus, std::string stringToConvert) : status(extStatus) {
-	if (extStatus == Status::memoryAllocationFailure) {
-		std::cout << "MEMORY ALLOCATION FALIURE";
-		status = Status::success;
-		return;
-	}
-	if (extStatus == Status::invalidParamValue) {
-		std::cout << "INVALID PARAMETER";
-		status = Status::success;
-		return;
-	}
-	length = static_cast<int>(stringToConvert.size());
-	seqChars = new char[length + 1];
-	for (int i = 0; i < length; i++) {
-		seqChars[i] = stringToConvert[i];
-	}
-	seqChars[length] = '\0';
-
-	if (seqChars == nullptr) {
-		extStatus = Status::memoryAllocationFailure;
-		return;
-	}
-};
-myString::myString(Status& extStatus,const myString& objToCopy) : status(extStatus){
-	length = objToCopy.length;
-	seqChars = new char[length + 1];
-	for (int i = 0; i < length; i++) {
-		seqChars[i] = objToCopy.seqChars[i];
-	}
-	seqChars[length] = '\0';
-
-	extStatus = objToCopy.status;
-
+myString::myString(Status& extStatus, std::string stringToConvert) : status(extStatus), seqChars(nullptr), length(0) {
+    if (extStatus == Status::memoryAllocationFailure) {
+        std::cout << "MEMORY ALLOCATION FAILURE" << std::endl;
+        status = Status::success;
+        return;
+    }
+    if (extStatus == Status::invalidParamValue) {
+        std::cout << "INVALID PARAMETER" << std::endl;
+        status = Status::success;
+        return;
+    }
+    length = static_cast<int>(stringToConvert.size());
+    seqChars = new char[length + 1];
+    if (seqChars == nullptr) {
+        extStatus = Status::memoryAllocationFailure;
+        return;
+    }
+    for (int i = 0; i < length; i++) {
+        seqChars[i] = stringToConvert[i];
+    }
+    seqChars[length] = '\0';
 }
+
+myString::myString(Status& extStatus, const myString& objToCopy) : status(extStatus), seqChars(nullptr), length(objToCopy.length) {
+    seqChars = new char[length + 1];
+    if (seqChars == nullptr) {
+        extStatus = Status::memoryAllocationFailure;
+        return;
+    }
+    for (int i = 0; i < length; i++) {
+        seqChars[i] = objToCopy.seqChars[i];
+    }
+    seqChars[length] = '\0';
+    extStatus = objToCopy.status;
+}
+
 myString::~myString() {
-	delete[] seqChars;
+    delete[] seqChars;
 }
 
 int myString::size() {
-	return length;
-};
+    return length;
+}
 
 void myString::addStart(myString& inputStringObj) {
-	int newLength = length + inputStringObj.size();
-	char* newArr = new char[newLength + 1];
-
-	for (int i = 0; i < inputStringObj.length; i++) {
-		newArr[i] = inputStringObj.seqChars[i];
-	}
-
-	for (int j = 0; j < length; j++) {
-		newArr[inputStringObj.length + j] = seqChars[j];
-	}
-
-	newArr[newLength] = '\0';
-	delete[] seqChars;
-	seqChars = newArr;
-	length = newLength;
-};
+    int newLength = length + inputStringObj.size();
+    char* newArr = new char[newLength + 1];
+    if (newArr == nullptr) {
+        status = Status::memoryAllocationFailure;
+        return;
+    }
+    for (int i = 0; i < inputStringObj.length; i++) {
+        newArr[i] = inputStringObj.seqChars[i];
+    }
+    for (int j = 0; j < length; j++) {
+        newArr[inputStringObj.length + j] = seqChars[j];
+    }
+    newArr[newLength] = '\0';
+    delete[] seqChars;
+    seqChars = newArr;
+    length = newLength;
+}
 
 void myString::addEnd(myString& inputStringObj) {
-int newLength = length + inputStringObj.size();
-	char* newArr = new char[newLength + 1];
-
-	for (int i = 0; i < length; i++) {
-		newArr[i] = seqChars[i];
-	}
-
-	for (int j = 0; j < inputStringObj.length; j++) {
-		newArr[length + j] = inputStringObj.seqChars[j];
-	}
-
-	newArr[newLength] = '\0';
-	delete[] seqChars;
-	seqChars = newArr;
-	length = newLength;
-};
+    int newLength = length + inputStringObj.size();
+    char* newArr = new char[newLength + 1];
+    if (newArr == nullptr) {
+        status = Status::memoryAllocationFailure;
+        return;
+    }
+    for (int i = 0; i < length; i++) {
+        newArr[i] = seqChars[i];
+    }
+    for (int j = 0; j < inputStringObj.length; j++) {
+        newArr[length + j] = inputStringObj.seqChars[j];
+    }
+    newArr[newLength] = '\0';
+    delete[] seqChars;
+    seqChars = newArr;
+    length = newLength;
+}
 
 myString* myString::partString(int startPos, int length) {
-	if (startPos < 1 || startPos > this->length || length > this->length - startPos + 1 || seqChars == nullptr) {
-		status = Status::invalidParamValue;
-		return new myString(status, "");
-	}
-	startPos--;
-	char* newArr = new char[length + 1];
-	for (int i = 0; i < length; i++) {
-		newArr[i] = seqChars[startPos + i];
-	}
-	newArr[length] = '\0';
-	return new myString(status, newArr);
-};
+    if (startPos < 1 || startPos > this->length || length > this->length - startPos + 1 || seqChars == nullptr) {
+        status = Status::invalidParamValue;
+        return new myString(status, "");
+    }
+    startPos--;
+    char* newArr = new char[length + 1];
+    if (newArr == nullptr) {
+        status = Status::memoryAllocationFailure;
+        return nullptr;
+    }
+    for (int i = 0; i < length; i++) {
+        newArr[i] = seqChars[startPos + i];
+    }
+    newArr[length] = '\0';
+    return new myString(status, newArr);
+}
 
 myString* myString::replPartString(myString objToAdd, int startPos, int length) {
-	if (startPos < 1 || startPos > this->length || length > this->length - startPos) {
-		status = Status::invalidParamValue;
-		return new myString(status, "");
-	}
-
-	startPos--;
-
-
-	int newLength = this->length - length + objToAdd.length;
-
-
-	char* newCharArray = new char[newLength + 1];
-
-
-	for (int i = 0; i < startPos; ++i) {
-		newCharArray[i] = this->seqChars[i];
-	}
-
-
-	for (int i = 0; i < objToAdd.length; ++i) {
-		newCharArray[startPos + i] = objToAdd.seqChars[i];
-	}
-
-
-	for (int i = startPos + length; i < this->length; ++i) {
-		newCharArray[i - length + objToAdd.length] = this->seqChars[i];
-	}
-
-
-	newCharArray[newLength] = '\0';
-
-	return new myString(status, newCharArray);
-};
+    if (startPos < 1 || startPos > this->length || length > this->length - startPos) {
+        status = Status::invalidParamValue;
+        return new myString(status, "");
+    }
+    startPos--;
+    int newLength = this->length - length + objToAdd.length;
+    char* newCharArray = new char[newLength + 1];
+    if (newCharArray == nullptr) {
+        status = Status::memoryAllocationFailure;
+        return nullptr;
+    }
+    for (int i = 0; i < startPos; ++i) {
+        newCharArray[i] = this->seqChars[i];
+    }
+    for (int i = 0; i < objToAdd.length; ++i) {
+        newCharArray[startPos + i] = objToAdd.seqChars[i];
+    }
+    for (int i = startPos + length; i < this->length; ++i) {
+        newCharArray[i - length + objToAdd.length] = this->seqChars[i];
+    }
+    newCharArray[newLength] = '\0';
+    return new myString(status, newCharArray);
+}
 
 myString* myString::replWholeString(myString objToReplace) {
-	int newLength = objToReplace.length;
-	char* newArr = new char[newLength + 1];
-	for (int i = 0; i < objToReplace.length; i++) {
-		newArr[i] = objToReplace.seqChars[i];
-	}
-	newArr[newLength] = '\0';
-	delete[] seqChars;
-	return new myString(status, newArr);
-};
+    int newLength = objToReplace.length;
+    char* newArr = new char[newLength + 1];
+    if (newArr == nullptr) {
+        status = Status::memoryAllocationFailure;
+        return nullptr;
+    }
+    for (int i = 0; i < objToReplace.length; i++) {
+        newArr[i] = objToReplace.seqChars[i];
+    }
+    newArr[newLength] = '\0';
+    delete[] seqChars;
+    seqChars = newArr;
+    length = newLength;
+    return this;
+}
+
 int myString::compareString(myString stringToCompare) {
-	int sameChars = 0;
-	if (length > stringToCompare.length) {
-		for (int i = 0; i < stringToCompare.length; i++) {
-			if (seqChars[i] == stringToCompare.seqChars[i]) {
-				sameChars++;
-			}
-			
-		}
-		return sameChars + 1;
-	}
-	if (length < stringToCompare.length) {
-		for (int i = 0; i < stringToCompare.length; i++) {
-			if (seqChars[i] == stringToCompare.seqChars[i]) {
-				sameChars++;
-			}
+    int sameChars = 0;
+    if (length > stringToCompare.length) {
+        for (int i = 0; i < stringToCompare.length; i++) {
+            if (seqChars[i] == stringToCompare.seqChars[i]) {
+                sameChars++;
+            }
+        }
+        return sameChars + 1;
+    }
+    if (length < stringToCompare.length) {
+        for (int i = 0; i < length; i++) {
+            if (seqChars[i] == stringToCompare.seqChars[i]) {
+                sameChars++;
+            }
+        }
+        return -sameChars - 1;
+    }
+    return sameChars;
+}
 
-		}
-		return -sameChars - 1;
-	}
-};
 void myString::initString() {
-	length = 0;
-	delete[] seqChars;
-	seqChars = nullptr;
-	status = Status::success;
-
-};
+    length = 0;
+    delete[] seqChars;
+    seqChars = nullptr;
+    status = Status::success;
+}
 
 myString* myString::setString(std::string stringToConvert) {
-	return new myString(status, stringToConvert);
-};
+    return new myString(status, stringToConvert);
+}
 
-std::string myString::getString() const{
-	return seqChars;
-};
+std::string myString::getString() const {
+    return seqChars ? std::string(seqChars) : "";
+}
 
-void myString::printStringScreen(){
-
-		for (int i = 0; i < length; i++) {
-			std::cout << seqChars[i];
-		}
-		std::cout << std::endl;
-};
+void myString::printStringScreen() {
+    if (seqChars) {
+        for (int i = 0; i < length; i++) {
+            std::cout << seqChars[i];
+        }
+        std::cout << std::endl;
+    }
+}
 
 bool myString::numericString() {
-	int allowedChars = 0;
-	bool hasNums = false;
-	for (int i = 0; i < length; i++) {
-		if (seqChars[i] >= 48 && seqChars[i] <= 57) {
-			allowedChars++;
-			hasNums = true;
-		}
-		if (seqChars[i] == ' ' || seqChars[i] == '\t' || seqChars[i] == '.' || seqChars[i] == '+' || seqChars[i] == '-') {
-			allowedChars++;
-		}
-	}
-	return (allowedChars == length) && hasNums;
-};
+    int allowedChars = 0;
+    bool hasNums = false;
+    for (int i = 0; i < length; i++) {
+        if (seqChars[i] >= '0' && seqChars[i] <= '9') {
+            allowedChars++;
+            hasNums = true;
+        }
+        if (seqChars[i] == ' ' || seqChars[i] == '\t' || seqChars[i] == '.' || seqChars[i] == '+' || seqChars[i] == '-') {
+            allowedChars++;
+        }
+    }
+    return (allowedChars == length) && hasNums;
+}
+
 bool myString::alphabeticString() {
-	int allowedChars = 0;
-	for (int i = 0; i < length; i++) {
-		if ((seqChars[i] >= 65 && seqChars[i] <= 90) || (seqChars[i] >= 97 && seqChars[i] <= 122)) {
-			allowedChars++;
-		}
-		if (seqChars[i] == ' ' || seqChars[i] == '\t') {
-			allowedChars++;
-		}
-	}
-	return allowedChars == length;
-};
+    int allowedChars = 0;
+    for (int i = 0; i < length; i++) {
+        if ((seqChars[i] >= 'A' && seqChars[i] <= 'Z') || (seqChars[i] >= 'a' && seqChars[i] <= 'z')) {
+            allowedChars++;
+        }
+        if (seqChars[i] == ' ' || seqChars[i] == '\t') {
+            allowedChars++;
+        }
+    }
+    return allowedChars == length;
+}
