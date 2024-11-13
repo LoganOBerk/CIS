8846058842b3971253ecf91d32733544b227fe84
LoganOBerk <146039828+LoganOBerk@users.noreply.github.com>
2024-11-12 22:39:12 -0500

    added sorted list functionality

--- 5. Labs/9. Lab (Tuesday, November 12)/Lab (Tuesday, November 12)/Lab (Tuesday, November 12)/funcs.h
+++ 5. Labs/9. Lab (Tuesday, November 12)/Lab (Tuesday, November 12)/Lab (Tuesday, November 12)/funcs.h
@@ -20,10 +20,29 @@ struct Node {
 };
 
 
-void sortInLinkedList(Node*& nodeToAdd, Node* list1) {
+void sortInLinkedList(Node* nodeToAdd, Node*& list2) {
+	if (list2 == nullptr) {
+		list2 = nodeToAdd;
+		nodeToAdd->next = nullptr;
+		return;
+	}
 
+	if (nodeToAdd->record.recordNumber < list2->record.recordNumber) {
+		nodeToAdd->next = list2;
+		list2 = nodeToAdd;
+		return;
+	}
+
+	Node* current = list2;
+	while (current->next && current->next->record.recordNumber < nodeToAdd->record.recordNumber) {
+		current = current->next;
+	}
+
+	nodeToAdd->next = current->next;
+	current->next = nodeToAdd;
+	
 }
-bool doesRecordExist(int input, Node*& list1) {
+bool doesRecordExist(int input, Node*& list1, Node*& list2) {
 	Node* current = list1;
 	while (current) {
 		if (current->record.recordNumber = input) {
@@ -38,7 +57,7 @@ void getValidRecordNumber(Record*& myRecord) {
 	int input;
 	while(true){
 		cin >> input;
-		if (cin.fail() || cin.peek() != '\n') {
+		if (cin.fail() || cin.peek() != '\n' || input > 100 || input < 1) {
 			cin.clear();
 			cin.ignore(numeric_limits<streamsize>::max(), '\n');
 			cout << "Invalid Record Number Try again: ";
@@ -116,9 +135,9 @@ void getValidCost(Record*& myRecord) {
 
 
 
-Record* collectRecordData(Node* list1) {
+Record* collectRecordData() {
 	Record* myRecord = new Record;
-	cout << "Enter a record number: ";
+	cout << "Enter a record number(1-100): ";
 	getValidRecordNumber(myRecord);
 	cout << "Enter a tool: ";
 	getValidTool(myRecord);
@@ -131,7 +150,7 @@ Record* collectRecordData(Node* list1) {
 	return myRecord;
 }
 
-void placeInLinkedList(Node*& nodeToAdd, Node*& list1) {
+void placeInLinkedList(Node* nodeToAdd, Node*& list1) {
 	if (!list1) {
 		list1 = nodeToAdd;
 		nodeToAdd->next = nullptr;
@@ -142,21 +161,25 @@ void placeInLinkedList(Node*& nodeToAdd, Node*& list1) {
 	}
 }
 
-void addRecord(Node*& list1) {
+void addRecord(Node*& list1, Node*& list2) {
 	bool recordExists = false;
 	Node* current = list1;
-	Node* node = new Node;
-	node->record = *collectRecordData(list1);
+	Node* node1 = new Node;
+	Node* node2 = new Node;
+	node1->record = *collectRecordData();
+	node2->record = node1->record;
 	while (current) {
-		if (current->record.recordNumber == node->record.recordNumber) {
-			delete node;
+		if (current->record.recordNumber == node1->record.recordNumber ) {
+			delete node1;
+			delete node2;
 			recordExists = true;
 			break;
 		}
 		current = current->next;
 	}
 	if (!recordExists) {
-		placeInLinkedList(node, list1);
+		placeInLinkedList(node1, list1);
+		sortInLinkedList(node2, list2);
 	}
 	else {
 		cout << "===================================================" << endl;
@@ -165,7 +188,7 @@ void addRecord(Node*& list1) {
 	}
 }
 
-void updateRecord(Node*& list1) {
+void updateRecord(Node*& list1, Node*& list2) {
 	cout << "Which record would you like to update? : ";
 	int recordNum = getValidRecordNumber();
 	Node* current = list1;
@@ -176,16 +199,17 @@ void updateRecord(Node*& list1) {
 		current = current->next;
 	}
 	if (current != nullptr && current->record.recordNumber == recordNum) {
-		current->record = *collectRecordData(list1);
+		current->record = *collectRecordData();
+		list2->record = current->record;
 	}
+	
 }
-void deleteRecord(Node*& list1) {
+void deleteRecord(Node*& list1, Node*& list2) {
 	cout << "Which record would you like to delete? : ";
-	int recordNum = getValidRecordNumber();  
+	int recordNum = getValidRecordNumber(); 
+
 	Node* current = list1;
 	Node* prev = nullptr;
-
-	
 	if (!current) {
 		cout << "No records to delete!" << endl;
 		return;
@@ -207,15 +231,59 @@ void deleteRecord(Node*& list1) {
 	else {
 		prev->next = current->next;  
 	}
+	delete current;
+	delete prev;
+
+	current = list2;
+	prev = nullptr;
+	if (!current) {
+		cout << "No records to delete!" << endl;
+		return;
+	}
+
+	while (current != nullptr && current->record.recordNumber != recordNum) {
+		prev = current;
+		current = current->next;
+	}
+
+
+	if (current == nullptr) {
+		cout << "Record not found!" << endl;
+		return;
+	}
+	if (prev == nullptr) {
+		list2 = current->next;
+	}
+	else {
+		prev->next = current->next;
+	}
 
 	delete current;
 	cout << "Record deleted!" << endl;
 }
-void displayRecord(Node* list1) {
+void displayRecord(Node* list1, Node* list2) {
 	cout << "Which record would you like to access? : ";
 	int recordNum = getValidRecordNumber();
+	cout << "\nContents of List1" << endl;
+	cout << "---------------------" << endl;
 	Node* current = list1;
 	while (current != nullptr && current->record.recordNumber != recordNum ) {
+		current = current->next;
+	}
+	if (current) {
+		cout << "Record #: " << current->record.recordNumber << endl;
+		cout << "Tool: " << current->record.toolname << endl;
+		cout << "Quantity: " << current->record.quantity << endl;
+		cout << "Cost: $" << current->record.cost << endl;
+	}
+	else {
+		cout << "Record does not exist!" << endl;
+	}
+	cout << "\nContents of List2" << endl;
+	cout << "---------------------" << endl;
+
+	current = list2;
+	while (current != nullptr && current->record.recordNumber != recordNum) {
 		current = current->next;
 	}
 	if (current) {
@@ -228,16 +296,35 @@ void displayRecord(Node* list1) {
 		cout << "Record does not exist!" << endl;
 	}
 }
-void displayTools(Node* list1) {
+void displayTools(Node* list1, Node* list2) {
+	cout << "\nContents of unsorted list" << endl;
+	cout << "---------------------" << endl;
 	Node* current = list1;
 	if (!current) {
 		cout << "No Records to read from!";
 	}
 	while (current) {
-		cout << current->record.toolname;
-		if (current->next) {
-			cout << ", ";
-		}
+		cout << "Record #: " << current->record.recordNumber << endl;
+		cout << "Tool: " << current->record.toolname << endl;
+		cout << "Quantity: " << current->record.quantity << endl;
+		cout << "Cost: " << current->record.cost << endl;
+		cout << endl;
+		current = current->next;
+	}
+	cout << endl;
+
+	cout << "\nContents of sorted list" << endl;
+	cout << "---------------------" << endl;
+	current = list2;
+	if (!current) {
+		cout << "No Records to read from!";
+	}
+	while (current) {
+		cout << "Record #: " << current->record.recordNumber << endl;
+		cout << "Tool: " << current->record.toolname << endl;
+		cout << "Quantity: " << current->record.quantity << endl;
+		cout << "Cost: " << current->record.cost << endl;
+		cout << endl;
 		current = current->next;
 	}
 	cout << endl;
@@ -246,36 +333,37 @@ void quit(bool& isRunning) {
 	isRunning = false;
 }
 
-void collectUserInput(bool& isRunning, Node* list1) {
+void collectUserInput(bool& isRunning, Node* list1, Node* list2) {
 
 	int input;
 	do{
-		cout << "What would you like to do?" << endl;
-		cout << "--------------------------" << endl;
+		cout << "===========Hardware Store Inventory============" << endl;
 		cout << " 1. Add a Record" << endl;
 		cout << " 2. Update a Record" << endl;
 		cout << " 3. Delete a Record" << endl;
 		cout << " 4. List a Record" << endl;
 		cout << " 5. List All Tools" << endl;
 		cout << " 6. Quit" << endl;
-		cout << "Enter Valid Choice Here: ";
+		cout << "===============================================" << endl;
+		cout << "Enter Valid Choice Here (1-6): ";
+		
 
 		cin >> input;
 		switch (input) {
 		case 1:
-			addRecord(list1);
+			addRecord(list1, list2);
 			break;
 		case 2:
-			updateRecord(list1);
+			updateRecord(list1, list2);
 			break;
 		case 3:
-			deleteRecord(list1);
+			deleteRecord(list1, list2);
 			break;
 		case 4:
-			displayRecord(list1);
+			displayRecord(list1, list2);
 			break;
 		case 5:
-			displayTools(list1);
+			displayTools(list1, list2);
 			break;
 		case 6:
 			quit(isRunning);
--- 5. Labs/9. Lab (Tuesday, November 12)/Lab (Tuesday, November 12)/Lab (Tuesday, November 12)/lab.cpp
+++ 5. Labs/9. Lab (Tuesday, November 12)/Lab (Tuesday, November 12)/Lab (Tuesday, November 12)/lab.cpp
@@ -4,6 +4,6 @@ int main() {
 	bool running = true;
 	Node* list1 = nullptr;
 	Node* list2 = nullptr;
-	collectUserInput(running, list1);
+	collectUserInput(running, list1, list2);
 	return 0;
 }
