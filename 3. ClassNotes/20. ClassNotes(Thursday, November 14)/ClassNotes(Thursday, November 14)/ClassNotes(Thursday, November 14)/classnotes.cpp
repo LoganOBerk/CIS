f7706ff69f78cfb2595522a7306869a5f3433a30
LoganOBerk <146039828+LoganOBerk@users.noreply.github.com>
2024-11-14 12:14:14 -0500

    classnotes

---
+++ 3. ClassNotes/20. ClassNotes(Thursday, November 14)/ClassNotes(Thursday, November 14)/ClassNotes(Thursday, November 14)/classnotes.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+int main() {
+
+	//Difference between ADT and data structure
+	//data type is what it is, data structure is how it works
+	//
+	// STACK
+	// only can take from top of stack and can only insert at top
+	//
+	// A ordered group of homogeneous items int which removal and addition of items can take place at the top
+	//LIFO structure last in first out
+	//
+	//uses of stack
+	//	undo/redo funcs
+	//	storing browsing history
+	//	prefix/infix/postfix conversions
+	//	expression evaluation
+	//	parsing
+	//	implementation of function calls
+	//	backtracking
+	//
+	//operations
+	//	MakeEmpty
+	//	IsEmpty
+	//	IsFull
+	//	Push(Itemtype newItem)
+	//	Pop
+	//	ItemType Top
+	//
+	//STL impliments stack for us but we cant
+	//
+	//QUEUES & Implementation
+	//	ordered group of homogeneous items
+	//	new elements are added at one end and elements are remove from front
+	//  FIFO first in first out
+	// 
+	//operations
+	//	MakeEmpty
+	//	IsEmpty
+	//	IsFull
+	//	Enqueue(push)
+	//	Dequeue(a little more complicated than pop)
+	//
+	//Can front be fixed
+	//	array implementation
+	//	linked list implementation
+	//	circular linked list
+	//	circular linked list implementation
+	//
+	//priority queue stores priority of task
+	//
+	//simulations attempt to mimic real scenarios and find best results
+	//time is zero at beginning of simulation.
+	//Event Driven simulation: used to generate effects that reflect real world
+
+	return 0;
+}
