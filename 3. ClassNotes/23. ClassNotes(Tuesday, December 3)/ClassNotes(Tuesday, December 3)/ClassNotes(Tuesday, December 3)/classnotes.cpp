556e9d5c84f7de9cc10db05a7c7dd17515310ee5
LoganOBerk <146039828+LoganOBerk@users.noreply.github.com>
2024-12-03 12:06:22 -0500

    classnotes

---
+++ 3. ClassNotes/23. ClassNotes(Tuesday, December 3)/ClassNotes(Tuesday, December 3)/ClassNotes(Tuesday, December 3)/classnotes.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+int main() {
+
+	//Binary Tree
+	//	each node can have at most 2 children
+	//  a unique path exists from the root to every node
+	//	root node is the top where we start
+	//  leaf nodes is the bottom most layer
+	//  starts at level 0 the root
+	//		goes down as far as tree goes
+	//	implementation
+	//		left child right child in a linked list format
+	//	variation
+	//		binary search tree
+	//			each node is a distinct value
+	//			the key values in the tree can be compared using greater than less than
+	//			a comparison is drawn each time to determine what side to insert on
+	//			
+	//		inorder LNR
+	//		postorder NLR
+	//		preorder LRN
+	//
+	//		as an array
+	//		index 0 is top of tree
+	//		remember formulas for array
+	//full binary tree has the full triangle every nonleaf as exatly two children
+	//complete binary tree is either full or full through the next-to-last level with leaves on the last level as far to the left as possible
+	//
+	//
+	//Balancing a tree
+	//	AVL Tree
+	//	Adelson-Velskii and Landis' tree
+	//	self balancing tree
+	//	assign a balance factor that auto balances the tree
+	//
+
+	return 0;
+}
