6fd864a1dc20ed9e034d5b87b4bc64267f29a121
LoganOBerk <146039828+LoganOBerk@users.noreply.github.com>
2024-12-05 12:15:38 -0500

    classnotes

---
+++ 3. ClassNotes/ClassNotes(Thursday, December 5)/ClassNotes(Thursday, December 5)/classnotes.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	//AVL tree is a self sorting binary search tree with limits at +-2 before balancing
+	//How many binary tree definitions
+	//	leaf node has no children
+	//	full tree has 0 or two children per node and balanced in a triangle
+	//	complete tree 
+	//	binary tree
+	//	binary search tree ordered
+	//	
+	//  three ways of printing
+	//	preorder NLR
+	//  inorder LRN
+	//	postorder LNR
+	//
+	//	traverse tree using that order
+	//	KNOW STANDARD METHODS
+	//  KNOW FORMULA FOR PLACING VALUE IN ARRAY
+	//
+	//	KNOW DEPTH FIRST
+	//	KNOW BREDTH FIRST
+
+	//Heaps
+	//	 a heap is a complete binary tree that is either empty and root contains a value thats greater than each of its children and had heaps as subtrees
+	//	left/right is not specified
+	//
+	//heap by default is maxheap can also be a minheap 
+	//removing from a heap
+	//	if we remove a  parent node we have a disjoint heap
+	//	then we heapify the disjoint heaps
+	//  
+	//if you know maximum number of items in priority queue heap is better
+	//
+	//selection sort with unsorted portion being reheaped
+
+	//What is a heap
+	//	must be a complete binary tree
+	//	for each node value must be greater than children
+	//
+
+	//REVIEW
+	//pre prints all the way left then all the way right acending then back down left when reaches head
+	//post prints left of node right of node then node and keeps going
+	//in order left all the way down print node then print node and print right and keep going recursively
+	//
+
+	return 0;
+}
