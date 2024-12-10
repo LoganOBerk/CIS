e5f1ebd3b7929d10eeeb7e50c9c0cc48aff6d106
LoganOBerk <146039828+LoganOBerk@users.noreply.github.com>
2024-12-10 12:16:23 -0500

    classnotes

---
+++ 3. ClassNotes/26. ClassNotes(Tuesday, December 10)/ClassNotes(Tuesday, December 10)/ClassNotes(Tuesday, December 10)/classnotes.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+
+int main() {
+
+	//FINAL EXAM
+	//know manns rules
+	//how to declare a friend
+	//brush up on classes
+	//friend is one directional
+	//inheritance
+	//complete tree full tree binary tree
+	//inheritance priority
+	//how to make a template implementation
+	//pointer midterm examples
+	//array memory access
+	//int x[10];
+	//int* ptr = x;
+	//          &x[0]
+	//           x[0]
+	//bytes for basic variables
+	//why would you need a pointer to a pointer?
+	//	when you want to pass by refrence in a function directly you can pass with ** and then you can change the actual value pass by refrence
+	//
+	//variations of linked lists circular(can only go forward), doubly linked circular(can also go backward)
+	//insert, delete, traversal of linked lists
+	//know node structure for doubly and regular linked list
+	//can make linked list dynamic
+	//formulas for each node in array
+	//	C  L 2*i + 1
+	//  C  R 2*i + 2
+	//  P (i-1)/2
+	//
+	//defn binary tree 0,1,2 children
+	//defn bin search tree
+	//complete only gaps are on the right only missing children are on the right lowest level full only missing are on lowest level
+	//full creates a triangle 
+	//
+	//ADTs stack, list, heap, queue, priority queue(queue within a queue)
+	//Know standard functions for ADTs
+	//	add
+	//	delete
+	//	clear
+	//	binary(check if full or empty)
+	//when does early binding take place, when does late
+	//dynamic memory is late binding at runtime
+	//early binding is compiletime
+	//UML(unified modeling language)
+	//3 parts
+	//name of class
+	//attributes
+	//methods
+	// public + 
+	// private -
+	// protected #
+	//
+	//pre     NLR
+	//post    LRN
+	//inorder LNR
+	//
+	//types of sorts
+	//
+	//basic recursion
+	//
+	//AVL basics
+	// max weight is +-1
+	// after 2 rebalance
+	//
+	//list unordered collection of items
+	//
+	//hashing assigning an index to a value based on its data, problem is collisions
+	//ideally has even distribution
+	//
+	//basic binary search O(logn)
+
+	return 0;
+}
