d1c0fb9f9fab0803af7d53ba9642d52a3d0eb8d5
Logan Berk <146039828+LoganOBerk@users.noreply.github.com>
2026-01-24 17:59:34 -0500

    some variable name changes and cleanups

--- CIS479/Project1/main.cpp
+++ CIS479/Project1/main.cpp
@@ -59,7 +59,7 @@ private:
 	int ii;
 public:
 	State();
-	State(State* p, int expO, int g, int h, int ii, int config[3][3]);
+	State(State*, int, int, int, int, int[3][3]);
 	
 	void setConfig(const int[3][3]);
 	void printState();
@@ -153,20 +153,18 @@ class Agent {
 private:
 	State init;
 	State goal;
-	int stateCount;
 	std::priority_queue<State*, std::vector<State*>, State::Comparator> frontier;
 	std::stack<State*> solutionSet;
 	std::unordered_map<State*, int, State::StateHash> exploredSet;
 	std::vector<State*> allocatedMem;
 	
+	int insertionIndex;
 	int heuristic(int[3][3]);
 public:
-	Agent(int initConfig[3][3], int goalConfig[3][3]);
+	Agent(int[3][3], int[3][3]);
 	~Agent();
 	void findShortestPath();
-	void genChild(State* p, std::string d);
-	State getInit();
-	State getGoal();
+	void genChild(State*, std::string);
 	void setInit(int[3][3]);
 	void setGoal(int[3][3]);
 	void printSolutionSet();
@@ -181,12 +179,39 @@ Agent::~Agent() {
 		delete alloc;
 	}
 }
-State Agent::getInit() {
-	return this->init;
+
+int Agent::heuristic(int config[3][3]) {
+	const int(&goalConfig)[3][3] = goal.getConfig();
+	int totalManhattan = 0;
+	int x = 0;
+	int y = 0;
+	int againstWind = 3;
+	int withWind = 1;
+	int sideWind = 2;
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			if (config[i][j] == 0) continue;
+			x = locX(config[i][j], goalConfig) - locX(config[i][j], config);
+			y = locY(config[i][j], goalConfig) - locY(config[i][j], config);
+
+			if (x < 0) {
+				x = abs(x);
+				x *= withWind;
+			}
+			else if (x > 0) {
+				x *= againstWind;
+			}
+
+			if (y != 0) {
+				y = abs(y);
+				y *= sideWind;
+			}
+			totalManhattan += (x + y);
+		}
+	}
+	return totalManhattan + tilesOutOfPlace(config, goalConfig);
 };
-State Agent::getGoal() {
-	return this->goal;
-};
+
 void Agent::setGoal(int goalConfig[3][3]) {
 	goal = State(nullptr, 1, 0, 0, 0, goalConfig);
 }
@@ -194,35 +219,9 @@ void Agent::setInit(int initConfig[3][3]) {
 	init = State(nullptr, 1, 0, heuristic(initConfig), 0, initConfig);
 }
 
-void Agent::findShortestPath() {
-	State* n = &init;
-	int expansionOrder = 1;
-	
-	while (true) {
-		n->expO = expansionOrder++;
-
-		if (*n == goal) break;
-
-		if (n->eX > 1) genChild(n, "LEFT");
-		if (n->eX < 3) genChild(n, "RIGHT");
-		if (n->eY > 1) genChild(n, "UP");
-		if (n->eY < 3) genChild(n, "DOWN");
-
-		n = frontier.top();
-		frontier.pop();
-		
-		exploredSet[n] = n->g;
-	}
-
-	while (*n != init) {
-		solutionSet.push(n);
-		n = n->p;
-	}
-};
-
 void Agent::genChild(State* p, std::string d) {
 	State* n = new State(*p);
-	n->ii = stateCount++;
+	n->ii = insertionIndex++;
 	n->p = p;
 	if (d == "LEFT") {
 		n->g++;
@@ -263,6 +262,34 @@ void Agent::genChild(State* p, std::string d) {
 	allocatedMem.push_back(n);
 	
 }
+
+
+void Agent::findShortestPath() {
+	State* n = &init;
+	int expansionOrder = 1;
+
+	while (true) {
+		n->expO = expansionOrder++;
+
+		if (*n == goal) break;
+
+		if (n->eX > 1) genChild(n, "LEFT");
+		if (n->eX < 3) genChild(n, "RIGHT");
+		if (n->eY > 1) genChild(n, "UP");
+		if (n->eY < 3) genChild(n, "DOWN");
+
+		n = frontier.top();
+		frontier.pop();
+
+		exploredSet[n] = n->g;
+	}
+
+	while (*n != init) {
+		solutionSet.push(n);
+		n = n->p;
+	}
+};
+
 void Agent::printSolutionSet() {
 	init.printState();
 	while (!solutionSet.empty()) {
@@ -270,43 +297,14 @@ void Agent::printSolutionSet() {
 		solutionSet.pop();
 	}
 };
-int Agent::heuristic(int config[3][3]) {
-	const int (& goalConfig)[3][3] = goal.getConfig();
-	int totalManhattan = 0;
-	int x = 0;
-	int y = 0;
-	int againstWind = 3;
-	int withWind = 1;
-	int sideWind = 2;
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			if (config[i][j] == 0) continue;
-			x = locX(config[i][j], goalConfig) - locX(config[i][j], config);
-			y = locY(config[i][j], goalConfig) - locY(config[i][j], config);
 
-			if (x < 0) {
-				x = abs(x);
-				x *= withWind;
-			}
-			else if (x > 0) {
-				x *= againstWind;
-			}
 
-			if (y != 0) {
-				y = abs(y);
-				y *= sideWind;
-			}
-			totalManhattan += (x + y);
-		}
-	}
-	return totalManhattan + tilesOutOfPlace(config, goalConfig);
-};
 
 int main() {
 	int initConfig[3][3] = { { 1,6,2 },{ 5,7,8 },{ 0,4,3 } };
 	int goalConfig[3][3] = { { 7,8,1 },{ 6,0,2 },{ 5,4,3 } };
-	Agent a(initConfig, goalConfig);
 
+	Agent a(initConfig, goalConfig);
 	a.findShortestPath();
 	a.printSolutionSet();
 	
